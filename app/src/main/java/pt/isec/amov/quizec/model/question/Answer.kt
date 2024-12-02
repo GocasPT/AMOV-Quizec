f202a89e6126553c6d8627cf595c8fbc6b68a41a
brunuhamaduh <brunuhamaduh@gmail.com>
2024-12-02 23:10:49 +0000

    True/False + Single Choice question creation + verifications

--- app/src/main/java/pt/isec/amov/quizec/model/question/Answer.kt
+++ app/src/main/java/pt/isec/amov/quizec/model/question/Answer.kt
@@ -1,9 +1,14 @@
 package pt.isec.amov.quizec.model.question
 
 sealed class Answer {
-    data class TrueFalse(val answer: Boolean) : Answer() {
+    data class TrueFalse(val rightAnswer: Boolean) : Answer() {
         override fun toString(): String {
-            return if (answer) "True" else "False"
+            return if (rightAnswer) "True" else "False"
+        }
+    }
+    data class SingleChoice(val answers: Set<String>, val rightAnswer: String) : Answer() {
+        override fun toString(): String {
+            return rightAnswer
         }
     }
 }
--- app/src/main/java/pt/isec/amov/quizec/ui/screens/CreateQuestionScreen.kt
+++ app/src/main/java/pt/isec/amov/quizec/ui/screens/CreateQuestionScreen.kt
@@ -1,13 +1,17 @@
 package pt.isec.amov.quizec.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import pt.isec.amov.quizec.model.question.Answer
@@ -23,8 +27,9 @@ fun CreateQuestionScreen(
     var questionTitle by remember { mutableStateOf("") }
     var questionType by remember { mutableStateOf<QuestionType>(QuestionType.YES_NO) }
     var questionAnswers by remember { mutableStateOf<Answer>(TrueFalse(false)) }
-
     var isExpanded by remember { mutableStateOf(false) }
+    val scrollState = rememberScrollState()
+    var saveEnabled by remember { mutableStateOf(true) }
 
     Column(
         modifier = Modifier
@@ -85,21 +90,78 @@ fun CreateQuestionScreen(
 
         when (questionType) {
             QuestionType.YES_NO -> {
+                var trueFalseAnswer by remember { mutableStateOf(false) }
+
+                LaunchedEffect(trueFalseAnswer) {
+                    questionAnswers = TrueFalse(trueFalseAnswer)
+                    saveEnabled = true
+                }
+
                 Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text("True")
-                    Switch(
-                        checked = (questionAnswers as TrueFalse).answer,
-                        onCheckedChange = { questionAnswers = TrueFalse(it) }
-                    )
                     Text("False")
+                    Switch(
+                        checked = trueFalseAnswer,
+                        onCheckedChange = { trueFalseAnswer = it }
+                    )
+                    Text("True")
                 }
             }
-            else -> {
+            QuestionType.SINGLE_CHOICE -> {
+                var newAnswer by remember { mutableStateOf("") }
+                var answers = remember { mutableStateSetOf<String>() }
+                var rightAnswer by remember { mutableStateOf("") }
 
+                LaunchedEffect(answers, rightAnswer) {
+                    questionAnswers = Answer.SingleChoice(answers, rightAnswer)
+                    saveEnabled = answers.size >= 2 && rightAnswer.isNotBlank()
+                }
+
+                LaunchedEffect(answers) {
+                    if (rightAnswer.isNotBlank() && !answers.contains(rightAnswer)) rightAnswer = ""
+                }
+
+                Row(
+                    modifier = Modifier.padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    OutlinedTextField(
+                        value = newAnswer,
+                        onValueChange = { newAnswer = it },
+                        label = { Text("Add Option") },
+                        modifier = Modifier.weight(1f)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(
+                        onClick = {
+                            answers.add(newAnswer)
+                            newAnswer = ""
+                        },
+                        enabled = newAnswer.isNotBlank()
+                    ) {
+                        Text("+")
+                    }
+                }
+                AnswerEntry(
+                    answers = answers,
+                    onClick = { rightAnswer = it },
+                    onOptionDelete = { answer ->
+                        answers.remove(answer)
+                        if (answer == rightAnswer) {
+                            rightAnswer = ""
+                        }
+                    },
+                    rightAnswer = rightAnswer,
+                    modifier = Modifier
+                        .weight(1f)
+                        .verticalScroll(scrollState)
+                )
+            }
+            else -> {
+                // TODO: All the other question types
             }
         }
 
@@ -107,9 +169,56 @@ fun CreateQuestionScreen(
             onClick = {
                 saveQuestion(Question(questionTitle, questionType, null, listOf(questionAnswers)))
             },
-            modifier = Modifier.padding(top = 16.dp)
+            modifier = Modifier.padding(top = 16.dp),
+            enabled = saveEnabled && questionTitle.isNotBlank()
         ) {
             Text("Save question")
+        }
+    }
+}
+
+@Composable
+fun AnswerEntry(
+    answers: Set<String>,
+    onClick: (String) -> Unit,
+    onOptionDelete: (String) -> Unit,
+    rightAnswer : String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ){
+        answers.forEach { answer ->
+            val backgroundColor = if (answer == rightAnswer) Color.Green else Color.Gray
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .clickable { onClick(answer) },
+                colors = CardDefaults.cardColors(
+                    containerColor = backgroundColor)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = answer,
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Button(
+                        onClick = { onOptionDelete(answer) }
+                    ) {
+                        Text("-")
+                    }
+                }
+            }
         }
     }
 }
