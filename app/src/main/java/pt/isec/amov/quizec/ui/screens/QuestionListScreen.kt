2cd3715fafb62b6d8f20985e63be72e2a7234778
GocasPT <gocasousa2013@gmail.com>
2024-12-02 11:56:35 +0000

    Refactory QuestionListScreen

--- app/src/main/java/pt/isec/amov/quizec/ui/screens/QuestionListScreen.kt
+++ app/src/main/java/pt/isec/amov/quizec/ui/screens/QuestionListScreen.kt
@@ -1,37 +1,42 @@
 package pt.isec.amov.quizec.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import pt.isec.amov.quizec.model.Question
+import pt.isec.amov.quizec.model.QuestionOption
+import pt.isec.amov.quizec.model.QuestionType
+import pt.isec.amov.quizec.ui.components.CustomList
 
 @Composable
 fun QuestionListScreen(
     questionList: List<Question>,
     onSelectQuestion: (Question) -> Unit,
 ) {
-    LazyColumn(
-        modifier = Modifier
-    ) {
-        items(
-            items = questionList,
-            key = { question -> question.hashCode() }
-        ) { question ->
-            QuestionCard(
-                question = question,
-                onSelectQuestion = onSelectQuestion
-            )
-        }
+    CustomList(
+        modifier = Modifier,
+        items = questionList,
+        onSelectItem = { question -> onSelectQuestion(question as Question) }
+    ) { question, onSelect ->
+        QuestionCard(
+            question = question as Question,
+            onSelectQuestion = { onSelect(question) }
+        )
     }
 }
 
@@ -55,21 +60,54 @@ fun QuestionCard(
         Column(
             modifier = Modifier
                 .padding(8.dp),
-        )  {
-            Text(
-                text = question.title,
-                fontSize = 20.sp
-            )
-            Text(
-                text = question.type.name,
-                fontSize = 16.sp
-            )
-            question.options.forEach { option ->
+        ) {
+            Row(
+                verticalAlignment = Alignment.Bottom,
+            ) {
+                question.image?.let {
+                    //TODO: get image from string and "return" image/file
+                    //PLACE_HOLDER
+                    Icon(Icons.Filled.AccountCircle, null)
+                }
+                Spacer(modifier = Modifier.padding(8.dp))
                 Text(
-                    text = option.text,
-                    fontSize = 12.sp
+                    text = question.title,
+                    fontSize = 20.sp
                 )
             }
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(
+                text = "Type ${question.type}",
+                fontSize = 16.sp
+            )
         }
     }
+}
+
+@Preview
+@Composable
+fun QLPreview() {
+    QuestionListScreen(
+        questionList = listOf(
+            Question(
+                "Question 1",
+                QuestionType.YES_NO,
+                null,
+                listOf(QuestionOption("Yes"), QuestionOption("No"))
+            ),
+            Question(
+                "Question 2",
+                QuestionType.MULTIPLE_CHOICE,
+                null,
+                listOf(QuestionOption("Option 1"), QuestionOption("Option 2"))
+            ),
+            Question(
+                "Question 3",
+                QuestionType.MULTIPLE_CHOICE,
+                "batata.jpg",
+                listOf(QuestionOption("Option 1"), QuestionOption("Option 2"))
+            )
+        ),
+        onSelectQuestion = {}
+    )
 }
