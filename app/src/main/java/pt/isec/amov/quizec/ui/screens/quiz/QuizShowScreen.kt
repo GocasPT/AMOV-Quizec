d469018324e386955dfe3be1d4ec75b6badb3f7a
Tiago Figueiredo <tiagofr.figueiredo@gmail.com>
2025-01-04 00:45:41 +0000

    add quiz show screen

--- app/src/main/java/pt/isec/amov/quizec/ui/screens/quiz/QuizShowScreen.kt
+++ app/src/main/java/pt/isec/amov/quizec/ui/screens/quiz/QuizShowScreen.kt
@@ -1,26 +1,262 @@
 package pt.isec.amov.quizec.ui.screens.quiz
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 import pt.isec.amov.quizec.model.quiz.Quiz
 
+@Preview(showBackground = true)
 @Composable
 fun QuizShowScreen(
-    quiz: Quiz,
+    quiz: Quiz = quizTeste.get(0),
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier,
+    var instantStart by remember { mutableStateOf(false) }
+
+    var isLocationRestricted by remember { mutableStateOf(false) }
+
+    var sliderLocation by remember { mutableStateOf(60f) }
+
+    var responseTimer by remember { mutableStateOf(10f) }
+
+    var isInstantScore by remember { mutableStateOf(true) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
     ) {
-        Text(
-            text = quiz.title,
-        )
-        Spacer(modifier = Modifier.padding(8.dp))
-        //TODO: Show all details
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Demo_ExposedDropdownMenuBox()
+        }
+
+
+//        Row(
+//            modifier = Modifier
+//                .fillMaxWidth(),
+//            horizontalArrangement = Arrangement.Center,
+//            verticalAlignment = Alignment.CenterVertically
+//        ) {
+//            DropdownMenu(
+//                expanded = true,
+//                onDismissRequest = { }
+//            ) {
+//                DropdownMenuItem(
+//                    text = { Text(text = "Location Restricted") },
+//                    onClick = { isLocationRestricted = !isLocationRestricted }
+//                )
+//                DropdownMenuItem(
+//                    text = { Text(text = "Instant Score") },
+//                    onClick = { isInstantScore = !isInstantScore }
+//                )
+//            }
+//        }
+
+        quiz.image?.let {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentDescription = null,
+                    alignment = Alignment.Center,
+                    contentScale = ContentScale.Crop,
+                    painter = rememberImagePainter(quiz.image),
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "QUIZ ID",
+                    fontSize = 42.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Normal,
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Instant Start",
+                )
+                Switch(
+                    checked = false,
+                    onCheckedChange = { instantStart = !instantStart }
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(2.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Quiz Time",
+                )
+                Slider(
+                    value = responseTimer,
+                    onValueChange = { responseTimer = it },
+                    valueRange = 0f..180f,
+                    steps = 18,
+                )
+                Text(
+                    text = "${responseTimer.toInt().toString()} s",
+                )
+            }
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Location Restricted",
+                )
+                Switch(
+                    checked = false,
+                    onCheckedChange = { isLocationRestricted = !isLocationRestricted }
+                )
+            }
+
+            isLocationRestricted.let {
+                if (it) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalArrangement = Arrangement.spacedBy(12.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Slider(
+                            value = sliderLocation,
+                            onValueChange = { sliderLocation = it },
+                            valueRange = 0f..100f,
+                            steps = 20,
+                        )
+                        Text(
+                            text = "${sliderLocation.toString()} km",
+                        )
+                    }
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Instant Score",
+                )
+                Switch(
+                    checked = false,
+                    onCheckedChange = { isInstantScore = !isInstantScore }
+                )
+            }
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Demo_ExposedDropdownMenuBox() {
+    val context = LocalContext.current
+    val coffeeDrinks = arrayOf("Americano", "Cappuccino", "Espresso", "Latte", "Mocha")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf(coffeeDrinks[0]) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = {
+                expanded = !expanded
+            }
+        ) {
+            TextField(
+                value = selectedText,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                modifier = Modifier.menuAnchor()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                coffeeDrinks.forEach { item ->
+                    DropdownMenuItem(
+                        text = { Text(text = item) },
+                        onClick = {
+                            selectedText = item
+                            expanded = false
+                            Toast.makeText(context, item, Toast.LENGTH_SHORT).show()
+                        }
+                    )
+                }
+            }
+        }
     }
 }
